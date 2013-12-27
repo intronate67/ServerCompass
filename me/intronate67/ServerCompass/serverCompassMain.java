db31d94dc4a45fb5d88092ca5f7818537a022890
Hunter Sharpe <huntersharpe@me.com>
2013-12-27 16:48:38 -0600

    Added stuff

--- me/intronate67/ServerCompass/serverCompassMain.java
+++ me/intronate67/ServerCompass/serverCompassMain.java
@@ -3,13 +3,18 @@ package me.intronate67.ServerCompass;
 import java.io.File;
 import java.util.logging.Logger;
 
-import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class ServerCompassMain extends JavaPlugin{
+public class ServerCompassMain extends JavaPlugin implements Listener{
 	
 	public final Logger logger = Logger.getLogger("Minecraft");
 	public int version = (int) 1.0;
@@ -27,10 +32,20 @@ public class ServerCompassMain extends JavaPlugin{
 	public void onDisable(){
 		getLogger().info("ServerCompass" + version + " has been disabled!");
 	}
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void onLogin(PlayerLoginEvent event){
+		
+	}
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String lable, String[] args){
 		Player player = (Player) sender;
-
+		if(cmd.getName().equalsIgnoreCase("getcompass")){
+			if(player.getInventory().contains(Material.COMPASS)){
+				player.sendMessage(ChatColor.RED + "You already have the compass.");
+			}else{
+				getServer().dispatchCommand(getServer().getConsoleSender(), "give " + sender + "345 1 1");
+			}
+		}
 		return false;
 	}
 }
